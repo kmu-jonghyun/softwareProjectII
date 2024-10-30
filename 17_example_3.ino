e36e28a412f7e132b2df8f6a5511fd11c14d2786
kmu-jonghyun <157869417+kmu-jonghyun@users.noreply.github.com>
2024-10-30 15:49:19 +0900

    Update 17_example_3.ino

--- 17_example_3.ino
+++ 17_example_3.ino
@@ -6,9 +6,9 @@
 #define PIN_LED   9
 #define PIN_SERVO 10
 
-#define _DUTY_MIN 1000  // servo full clock-wise position (0 degree)
-#define _DUTY_NEU 2000  // servo neutral position (90 degree)
-#define _DUTY_MAX 3000  // servo full counter-clockwise position (180 degree)
+#define _DUTY_MIN 2500  // servo full clock-wise position (0 degree)
+#define _DUTY_NEU 1500  // servo neutral position (90 degree)
+#define _DUTY_MAX 500  // servo full counter-clockwise position (180 degree)
 
 #define _DIST_MIN  100.0   // minimum distance 100mm
 #define _DIST_MAX  250.0   // maximum distance 250mm
@@ -48,10 +48,10 @@ void loop()
   dist_raw = ((6762.0 / (a_value - 9.0)) - 4.0) * 10.0;
 
   if (dist_raw >= _DIST_MIN && dist_raw <= _DIST_MAX) {
-  digitalWrite(PIN_LED, HIGH);
+  digitalWrite(PIN_LED, LOW);
   } 
   else {
-  digitalWrite(PIN_LED, LOW);
+  digitalWrite(PIN_LED, HIGH);
   }
   
   dist_ema = EMA_ALPHA * dist_raw + (1 - EMA_ALPHA) * dist_ema;// Put EMA filter code here
